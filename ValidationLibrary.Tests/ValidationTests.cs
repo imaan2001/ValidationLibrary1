50c3a8fb246bf0b3a1e213c57c82c53475fba121
Muhammed Imaan <muhammedimaan001@gmail.com>
2026-03-09 04:46:49 +0530

    Add unit tests project

---
+++ ValidationLibrary.Tests/ValidationTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationLibrary1.Core;
+using ValidationLibrary1.Rules;
+
+namespace ValidationLibrary1.Tests
+{
+
+    public class ValidationTests
+    {
+        [Fact]
+        public void RequiredRule_ShouldFail_WhenValueIsNull()
+        {
+            var rule = new RequiredRule("Field required");
+            var result = new ValidationResult();
+
+            rule.Validate("Username", null, result);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void MinLengthRule_ShouldFail_WhenTextTooShort()
+        {
+            var rule = new MinLengthRule(5);
+            var result = new ValidationResult();
+
+            rule.Validate("Username", "abc", result);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void MaxLengthRule_ShouldFail_WhenTextTooLong()
+        {
+            var rule = new MaxLengthRule(5);
+            var result = new ValidationResult();
+
+            rule.Validate("Username", "toolongtext", result);
+
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void EmailRule_ShouldPass_ForValidEmail()
+        {
+            var rule = new EmailRule();
+            var result = new ValidationResult();
+
+            rule.Validate("Email", "user@gmail.com", result);
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void RangeRule_ShouldFail_WhenOutsideRange()
+        {
+            var rule = new RangeRule(18, 60);
+            var result = new ValidationResult();
+
+            rule.Validate("Age", 70, result);
+
+            Assert.False(result.IsValid);
+        }
+    }
+}
